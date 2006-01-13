3ecceb028b1357291cbf2825d9193c5844e7fc31
jacmoe <jacmoe@4fa2dde5-35f3-0310-a95e-e112236e8438>
2006-01-13 16:27:44 +0000

    Fixed AppWizard80 resources.

--- trunk/tools/ogrewizard80/Files/Templates/1033/res/resource.h
+++ trunk/tools/ogrewizard80/Files/Templates/1033/res/resource.h
@@ -1,12 +1,12 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by root.rc
+// Used by [!output PROJECT_NAME].rc
 //
 #define IDI_ICON1                       101
 #define IDS_APP_TITLE                   103
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
