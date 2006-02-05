60f3cf2cdb7c6309c3f9386c098d63525afaa762
jacmoe <jacmoe@4fa2dde5-35f3-0310-a95e-e112236e8438>
2006-02-05 22:27:29 +0000

    Added standard log for OPCODE.

--- trunk/ogreopcode/src/OgreCollisionManager.cpp
+++ trunk/ogreopcode/src/OgreCollisionManager.cpp
@@ -3,28 +3,29 @@
 ///  @brief <TODO: insert file description here>
 ///
 ///  @author The OgreOpcode Team @date 28-05-2005
-///  
+///
 ///  This file is part of OgreOpcode.
-///  
+///
 ///  A lot of the code is based on the Nebula Opcode Collision module, see docs/Nebula_license.txt
-///  
+///
 ///  OgreOpcode is free software; you can redistribute it and/or
 ///  modify it under the terms of the GNU Lesser General Public
 ///  License as published by the Free Software Foundation; either
 ///  version 2.1 of the License, or (at your option) any later version.
-///  
+///
 ///  OgreOpcode is distributed in the hope that it will be useful,
 ///  but WITHOUT ANY WARRANTY; without even the implied warranty of
 ///  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ///  Lesser General Public License for more details.
-///  
+///
 ///  You should have received a copy of the GNU Lesser General Public
 ///  License along with OgreOpcode; if not, write to the Free Software
 ///  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-///  
+///
 ///////////////////////////////////////////////////////////////////////////////
 #include "OgreCollisionManager.h"
 #include "OgreCollisionReporter.h"
+
 
 template<> OgreOpcode::CollisionManager* Ogre::Singleton<OgreOpcode::CollisionManager>::ms_Singleton = 0;
 
@@ -46,6 +47,7 @@ namespace OgreOpcode
 		in_begin_collclasses  = false;
 		in_begin_colltypes    = false;
 
+		Opcode::InitOpcode();
 		// setup the tree collider
 		opcTreeCollider.SetFirstContact(false);       // report all contacts
 		opcTreeCollider.SetFullBoxBoxTest(false);     // use coarse BV-BV tests
@@ -92,6 +94,8 @@ namespace OgreOpcode
 			delete shpIter->second;
 		}
 		shape_list.clear();
+
+		Opcode::CloseOpcode();
 	}
 
 	CollisionContext *CollisionManager::newContext(const String& contextName)
